ed23f48b8d20514f0f2cfb5d469f2880c887f906
Mike <salmela.mike@gmail.com>
2018-07-23 19:18:44 +0300

    testing program functionality

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include "Wav/Wav.h"
 #include "SignalProcessing/Filter/FIR.h"
+#include "SignalProcessing/Audio/Audio.h"
 #include <fstream>
 #include <iostream>
+
 
 int main(){
 
 
-	Wav test = Wav("elvis.wav");
-	FIR fir = FIR(100, 0.1, HIGHPASS);
-
+	//Wav test = Wav("seiska.wav");
+	makeSine(5000, 20000, 100000);
+	Wav test2(makeSine(5000, 50000, 2000), 50000);
+	test2.writeWAV("test2.wav");
 	//test.changeDATA(fir.filter(test.get_RAW_data()));
 
-	test.writeWAV("test.wav");
+	//test.writeWAV("test.wav");
 
 	return 0;
 }
